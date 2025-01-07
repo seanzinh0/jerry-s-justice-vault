521f9d7afe11685f1a65430041e7757cb2cc5232
spichay <seanpichay1015@gmail.com>
2025-01-07 15:07:41 -0500

    template literals to show data on front end

--- public/js/searchCase.js
+++ public/js/searchCase.js
@@ -6,9 +6,22 @@ form.addEventListener('submit', (e) => {
     const searchInput = document.querySelector('#search-case').value;
     fetch(`http://localhost:3000/api/search?lawCase=${searchInput}`).then(response => response.json()).then(data => {
         if (data.error) {
-            console.log(data.error);
+            similarCases.innerHTML = data.error;
         } else {
-            console.log(data)
+            similarCases.innerHTML = '';
+            let result = '';
+            data.results.forEach(lawCase  => {
+                const {attorney, caseName, court, dateFiled, doc, snippet} = lawCase;
+                result += `<div class="case">
+                    <h3>${caseName}</h3>
+                    <p>Attorney: ${attorney}</p>
+                    <p>Court: ${court}</p>
+                    <p>Date Filed: ${dateFiled}</p>
+                    <p>Snippet: ${snippet}</p>
+                    <a href=${doc}>Case Document</a>
+                </div>`
+            });
+            similarCases.innerHTML = result;
         }
     })
 })
