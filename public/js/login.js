501598d20d5c875db769f39e498ac6e86d4b774f
YVictorin <vicyonjou@gmail.com>
2025-01-08 14:45:27 -0500

    wip: adding login client logic

--- public/js/login.js
+++ public/js/login.js
@@ -1,39 +1,35 @@
-// const form = document.querySelector(".form");
+const form = document.querySelector(".form");
+const userPassword = document.querySelector('#password').value.trim();
+const userEmail = document.querySelector('#email').value.trim();
 
-// const authUser = async (username, password) => {
-//     let isLoggedIn = false;
+const authUser = async (email, password) => {
+    let isLoggedIn = false;
 
-//     try {
-//         // Fetch user data
-//         const response = await fetch(`api/login?username=${encodeURIComponent(username)}`, {
-//             method: "GET",
-//             headers: { Accept: "application/json" },
-//         });
+    try {
+        // Fetch user data
+        const response = await fetch(`api/login`, {
+            method: "GET",
+            headers: { Accept: "application/json" },
+        });
 
-//         const userDataJSON = await response.json();
+        const userDataJSON = await response.json();
 
-//         // Validate password
-//         if (userDataJSON.password !== password) {
-//             alert("The password was incorrect");
-//             window.location.href = "/login";
-//             return;
-//         } else {
-//             isLoggedIn = true;
-//         }
+        // Validate password
+        if (userDataJSON.password !== password) {
+            alert("The password was incorrect");
+            window.location.href = "/login";
+            return;
+        } else {
+            isLoggedIn = true;
+        }
 
-//         // Update login status
-//         await fetch(`api/login?username=${encodeURIComponent(username)}&loggedIn=${isLoggedIn}`, {
-//             method: "PATCH",
-//             headers: { Accept: "application/json" },
-//         });
-
-//         alert("Login successful!");
-//     } catch (e) {
-//         console.error("Error authenticating user:", e);
-//     }
-// };
+        alert("Login successful!");
+    } catch (e) {
+        console.error("Error authenticating user:", e);
+    }
+};
 
 
-// form.addEventListener('submit', () => {
-//     authUser
-// })
+form.addEventListener('submit', () => {
+    authUser()
+})
