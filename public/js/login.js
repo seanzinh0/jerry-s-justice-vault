e54c2d0515f871a995a6bf21b7a3a76d2aa49faf
YVictorin <vicyonjou@gmail.com>
2025-01-10 10:45:45 -0500

    add custom modals to dispaly to users and responsiveness

--- public/js/login.js
+++ public/js/login.js
@@ -2,6 +2,10 @@ const form = document.querySelector(".login-form");
 const username = document.querySelector("#username");
 const password = document.querySelector("#password");
 
+const openButton = document.querySelector('[data-open-modal]')
+const closeButton = document.querySelector('[data-close-modal]')
+const modal = document.querySelector('dialog')
+
 async function processUserLogin() {
     try { 
         // Fetch user id
@@ -9,23 +13,34 @@ async function processUserLogin() {
             method: 'POST',
          })
            const result = await response.json();
-           //if user is already logged in and is trying to log in again with the same account 
-           if(localStorage.getItem('id') !== null && !result.error) {
-                alert('You are already logged in') 
-                return;  
-        }
 
-        //if username and password aren't in db
-        if(result.error) {
-            alert(result.error);
-            return;
-        } else {
+           //if user is already logged in and is trying to log in again with the same account 
+           if(localStorage.getItem('id') !== null) {
+            modal.innerHTML = `<p>You are already logged in, Press the link below to be taken to your account page<p><a href=/account>Your Account</a>`
+            modal.showModal()
+            return;  
+        }
+     
             localStorage.setItem('id', result.id)
-            alert("Login successful!");
-        }     
+            modal.textContent = 'Login successful!'
+            modal.showModal()
+            
+            setTimeout(() => {
+                modal.close()
+            }, 2000)
+      
 
     } catch (e) {
         console.error("Error fetching user id:", e);
+        //if username and password aren't in db
+        if(e) {
+            modal.textContent = 'No user exist with that information, Please try again.'
+            modal.showModal()
+
+            setTimeout(() => {
+                modal.close();
+            }, 2000)
+        }
     }
 }
    
