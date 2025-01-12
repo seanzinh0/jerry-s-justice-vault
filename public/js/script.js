372820e0ee24153fd9b05d738113a232606a4b41
YVictorin <vicyonjou@gmail.com>
2025-01-12 16:16:24 -0500

    refactor script.js to only hide login link in nav if user is logged in

--- public/js/script.js
+++ public/js/script.js
@@ -1,12 +1,9 @@
 const loginLink = document.querySelector('.menu li:nth-child(3)');
-const registerLink = document.querySelector('.menu li:nth-child(4)');
 
 const isUserLoggedIn = localStorage.getItem('id');
 
 if(isUserLoggedIn) {
     loginLink.style.display = 'none';
-    registerLink.style.display = 'none';
 } else {
     loginLink.style.display = 'inline';
-    registerLink.style.display = 'inline';
 }
